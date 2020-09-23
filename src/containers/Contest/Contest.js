40a77e2b5fe71c0dfef6e7257e4c951820e1acb2
JuanSevillano <sevi0_6@hotmail.com>
2020-09-23 11:39:16 +0200

    5 slots added to draw view and winner state when all Slots finish its scrolling

--- src/containers/Contest/Contest.js
+++ src/containers/Contest/Contest.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 
-import { ArrowLeftOutlined } from '@ant-design/icons'
+import { ArrowLeftOutlined, PlayCircleOutlined, ReloadOutlined } from '@ant-design/icons'
 import classes from './Contest.module.css'
 
 import Slot from 'react-slot-machine'
@@ -8,16 +8,27 @@ import Slot from 'react-slot-machine'
 const Contest = ({ teams, holes, back }) => {
 
 	const times = 2
-	const duration = 3000
+	const duration = 2000
 
-	const [target, setTarget] = useState(0)
+	const [target, setTarget] = useState([0, 0, 0, 0, 0])
 	const [turn, setTurn] = useState(false)
 
 
 	const startHandler = e => {
-		const _newTarget = Math.floor(Math.random() * (holes.length - 2)) + 1
-		setTarget(_newTarget)
-		setTurn(true)
+		const _targets = target.map(t => Math.floor(Math.random() * (holes.length - 2)) + 1)
+		setTarget(_targets)
+	}
+
+	const clearHandler = e => {
+		const _targets = [0, 0, 0, 0, 0]
+		setTarget(_targets)
+		setTurn(false)
+	}
+
+	const contestFinish = slot => {
+		if (slot === 4) {
+			setTurn(true)
+		}
 	}
 
 	return (
@@ -31,22 +42,30 @@ const Contest = ({ teams, holes, back }) => {
 						size="large" />
 					<h1> Ruffle</h1>
 				</div>
-				<button onClick={startHandler}>
-					Start
-				</button>
-				<Slot
-					times={times}
-					target={target}
-					duration={duration}
-					className={classes.Slot}>
+				<div className={classes.Controls}>
+					<PlayCircleOutlined style={{ fontSize: '2rem' }} onClick={startHandler} />
+					<ReloadOutlined style={{ fontSize: '2rem' }} onClick={clearHandler} />
+				</div>
+				<div className={!turn ? classes.Machine : [classes.Machine, classes.Animated].join(' ')}>
 					{
-						holes.map(hole =>
-							<div key={hole} style={{ width: '100%', height: '100%' }}>
-								{hole}
-							</div>
-						)
+						target.map((t, i) => (
+							<Slot
+								times={times}
+								target={target[i]}
+								duration={duration * (i + 1)}
+								onEnd={e => contestFinish(i)}
+								className={classes.Slot}>
+								{
+									holes.map(hole =>
+										<div key={hole} style={{ width: '100%', height: '100%' }}>
+											{hole}
+										</div>
+									)
+								}
+							</Slot>
+						))
 					}
-				</Slot>
+				</div>
 			</div>
 		</div >
 	)
